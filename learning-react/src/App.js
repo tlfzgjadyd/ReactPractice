8b985866dca8d51d51551c573d90e3a27916f9d5
tlfzgjadyd <tlfzgjadyd@naver.com>
2020-07-21 14:10:32 +0900

    + - 버튼 누를때마다 count값이 변하는 앱

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import HelloWorld from './Components/HelloWorld'
+import CounterExample from './Components/CounterExample'
 
 function App() {
   return (
    <div>
-     <HelloWorld name="tlfzg"/>
+     {/*<HelloWorld name="tlfzg"/>*/}
+     <CounterExample/>
    </div>
   );
 }
