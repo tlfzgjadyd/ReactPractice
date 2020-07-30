3516a405348b3a8cb31913cd3d6877dc9e23b375
tlfzgjadyd <tlfzgjadyd@naver.com>
2020-07-30 11:13:07 +0900

    뭐야 아무것도 안했는데 뭐가 바뀌어있네

--- learning-react/src/Views/Product.js
+++ learning-react/src/Views/Product.js
@@ -1,16 +1,9 @@
-import React  {useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 
 function Product(){
-    const url = 'https://복붙한값"
-    const [product, setProduct] =useState(null)
-    axios.get(url)
-	.them(response => {
-		setProduct(response.data)
-	})
     return(
         <div>
-            <h1>The product page</h1>
+            <h1>The product</h1>
         </div>
     );
 }
