47e387e729cb1c37616870ff95b3ea3586d07c6d
tlfzgjadyd <tlfzgjadyd@naver.com>
2020-09-11 16:03:08 +0900

    Weather주석판 추가

--- solve_problems/src/Components/Weather.jsx
+++ solve_problems/src/Components/Weather.jsx
@@ -1,13 +1,12 @@
 import React, { PureComponent } from 'react';
 import axios from "axios"
-import PropTypes from "prop-types"
 
 class Weather extends PureComponent {
-    constructor(props) {
-    super(props);
+    constructor() {
+    super();
     this.state={
-        transactions:[{
-        "coord": {
+    transactions:[{ 
+        "coord": {     
           "lon": -122.08,
           "lat": 37.39
         },
@@ -49,17 +48,16 @@ class Weather extends PureComponent {
         "id": 420006353,
         "name": "Mountain View",
         "cod": 200
-        }],
-        temp: 0,
-        SeoulTemp:0,
-        LondonTemp:0
-
-      };
-      this.method=this.method.bind(this);
+        }], 
+        temp: 0,       
+        SeoulTemp:0,  
+        LondonTemp:0  
+      };             
+      this.method=this.method.bind(this);  //constructor 안에는 state선언 밑에 함수 bind 필요
       this.getSeoul=this.getSeoul.bind(this);
       this.getLondon=this.getLondon.bind(this);
     }
-      method() {
+      method() {  //constructor 바깥에는 함수 정의부를 선언 
         this.setState({temp:this.state.temp+1});
       }
       getSeoul() {
@@ -71,12 +69,13 @@ class Weather extends PureComponent {
     componentDidMount() {   //제공되는 api앞에는 반드시 http같은걸 추가해야하며 값 제대로 들어왔는지 concole로 response 찍어봐야됨
         axios.get('http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=f951b19e378232992a50374991b1f518&units=metric&q=London')
         .then(response =>{
-            this.setState({temp:response.data.main.temp, LondonTemp:response.data.main.temp})
+            this.setState({temp:response.data.main.temp, LondonTemp:response.data.main.temp}) //이부분이 선언해둔 2개의 변수에 데이터 집어넣는 부분이다
+                        //하나의 setState 만으로도 여러개의 변수에 실체를 집어넣을 수 있음 this.setState({ , , })
             console.log(response.data.main.temp);//state 변수 사용시에는 this.temp말고 this.state.temp까지 해야함
         })
-        .then(
+        .then(  //axios를 두 번 쓸 경우 .then으로 이어줘야했음 이렇게 안하는 방법도 있을것같긴 한데 일단 에러 안나고 성공한건 이게 끝이다
         axios.get('http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=f951b19e378232992a50374991b1f518&units=metric&q=Seoul')
-        .then(response =>{
+        .then(response =>{ 
             this.setState({SeoulTemp:response.data.main.temp})
             console.log(this.state.SeoulTemp); //state 변수 사용시에는 this.temp말고 this.state.temp까지 해야함
         })
@@ -90,7 +89,7 @@ class Weather extends PureComponent {
                 <button onClick={this.method}>지구온난화 진행하기</button><br/><br/>
                 <button onClick={this.getSeoul}>서울 온도 가져오기</button> <br/><br/>
                 <button onClick={this.getLondon}>런던 온도 가져오기</button>
-            </div>
+            </div> //onClick함수 지정하는 부분에는 단순히 this.함수명 하면 되는게 아니라 {this.함수명} 이렇게 해야한다!
         );
     }
 }
---
+++ solve_problems/src/Components/WeatherByFetch.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class WeatherByFetch extends PureComponent {
+    render() {
+        return (
+            <div className="WeatherByFetch">
+                
+            </div>
+        );
+    }
+}
+
+export default WeatherByFetch;
