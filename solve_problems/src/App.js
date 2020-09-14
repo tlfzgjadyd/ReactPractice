714f199277c0c7104faa0c7198b06d3973f1e816
tlfzgjadyd <tlfzgjadyd@naver.com>
2020-09-14 15:15:34 +0900

    네이버페이 가맹점식별키 없어서 일단 전단계까지라도 시도해봄

--- solve_problems/src/App.js
+++ solve_problems/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Weather from "./Components/Weather"
 import WeatherByFetch from "./Components/WeatherByFetch"
+import NaverPay from "./Components/NaverPay"
 
 function App() {
   return (
     <div className="App">
-
+      <Weather/>
       <WeatherByFetch/>
+      <NaverPay/>
     </div>
   );
 }
